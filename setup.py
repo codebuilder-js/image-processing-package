685763bf37f305fbff5714fdee430a177a9225ac
Rafael Alves <codebuilder.js@gmail.com>
2025-04-09 14:24:28 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("requirements.txt") as f:
   requirements = f.read().splitlines()
 
 setup(
-  name="package_name",
-  version="0.0.1",
-  author="my_name",
-  author_email="my_email",
-  description="My short description",
+  name="image-processing-test",
+  version="0.0.2",
+  author="Rafael Correa de Lima",
+  author_email="rafael.script@gmail.com",
+  description="Image processing package. This project belongs to Karina Tiemi Kato, Tech Lead, Machine Learning Engineer, Data Scientist Specialist at Take. This package is a demo for simulation of upload on the Test Pypi website, and it's from the Bootcamp developer full stack Python class. E-mail: karinatkato@gmail.com.",
   long_description="text/markdown",
-  url="my_github_repository_projetct_link",
+  url="https://github.com/codebuilder-js/image-processing-package/",
   packages=find_packages(),
   install_requirements=requirements,
   python_requires='>=3.8',
